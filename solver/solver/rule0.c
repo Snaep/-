82ab1a88302ffe6d54833fe956ef79f542a322f6
Snaep <ksnaep@web.de>
2015-06-05 13:38:13 -0700

    implemented first rule

--- solver/solver/rule0.c
+++ solver/solver/rule0.c
@@ -1,5 +1,32 @@
 #include "rules.h"
 
-int rule0( struct Sudoku* sudoku, unsigned int x, unsigned  int y ) {
+//prüft ob ein Wert innerhalb einer Zeile
+//nur in der aktuellen Zelle möglich ist
+int rule0( struct Sudoku* sud, unsigned int x, unsigned  int y ) {
+	int i;
+	SudokuCell row;
+	
+	row = 0;
+	for( i = 0; i < sud->length; i++ ) {
+		if( i != x ) {
+			row |= sud->grid[y][i];
+		}
+	}
+
+	//loop through candidates
+	for( i = 1; i <= sud->length; i++ ) {
+
+		//wenn kandidat gefunden
+		if( ( sud->grid[y][x] & ( 1ll << i ) ) != 0 ) {
+
+			//wenn kandidat an keiner anderen Stelle in Reihe möglich ist
+			//Setze Zellwert
+			if( ( row & ( 1ll << i ) ) == 0 ) {
+				sud->pSetCell( sud, x, y, i );
+				return 1;
+			}
+		}
+	}
+
 	return 0;
 }
