4b621de73dc103ddfd787754390d7079bc929386
Snaep <ksnaep@web.de>
2015-06-08 12:41:26 -0700

    Update rule6.c

--- solver/solver/rule6.c
+++ solver/solver/rule6.c
@@ -1,16 +1,18 @@
 #include "rules.h"
 
-//beschreibung
+//naked pair row
 int rule6( struct Sudoku* sud, unsigned int x, unsigned int y ) {
-	int i, j;
+	unsigned int i, j;
 	if( __popcnt( sud->grid[y][x] ) != 2 ) return 0;
 
 	for( i = 0; i < sud->length; i++ ) {
 		if( i == x ) continue;
 		if( sud->grid[y][i] ^ sud->grid[y][x] == 0 ) {
 			for( j = 0; j < sud->length; j++ ) {
-				if( j != i && j != x ) sud->grid[y][j] &= stuff;
+				if( j != i && j != x ) sud->grid[y][j] &= ( ~( sud->grid[y][x] ) );
 			}
+
+			return 1;
 		}
 	}
 
