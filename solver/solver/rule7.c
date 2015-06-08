7658e1cb6157679afe7562db2b255f697304c8cc
Snaep <ksnaep@web.de>
2015-06-08 12:41:41 -0700

    Update rule7.c

--- solver/solver/rule7.c
+++ solver/solver/rule7.c
@@ -1,6 +1,20 @@
 #include "rules.h"
 
-//beschreibung
+//naked pair col
 int rule7( struct Sudoku* sud, unsigned int x, unsigned int y ) {
+	unsigned int i, j;
+	if( __popcnt( sud->grid[y][x] ) != 2 ) return 0;
+
+	for( i = 0; i < sud->length; i++ ) {
+		if( i == y ) continue;
+		if( sud->grid[i][x] ^ sud->grid[y][x] == 0 ) {
+			for( j = 0; j < sud->length; j++ ) {
+				if( j != i && j != y ) sud->grid[j][x] &= ( ~( sud->grid[y][x] ) );
+			}
+
+			return 1;
+		}
+	}
+
 	return 0;
 }
