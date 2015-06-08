e8e39ed4bb19f84ae637a05d32e325406dfeca8f
MMauser <Schmitz.marco@gmx.de>
2015-06-08 21:18:30 +0200

    trolol

--- solver/solver/rule3.c
+++ solver/solver/rule3.c
@@ -3,28 +3,15 @@
 int rule3 ( struct Sudoku* sud, unsigned int x, unsigned int y )
 {
 	unsigned int i, j;
-	unsigned int iTemp;
-	SudokuCell col;
-
-	col = 0;
-	for ( i = 0; i < sud->length; i++ )
-	{
-		if ( i != y )
-		{
-			col |= sud->grid[i][x];
-		}
-	}
 
 	for ( i = 0; i < sud->length; i++ )
 	{
 		if ( __popcnt ( ( sud->grid[y][x] ) & ( sud->grid[i][x] ) ) == 2 && i != y )
 		{
-			iTemp = i;
-			sud->grid[i][x] &= ( sud->grid[y][x] ) & ( sud->grid[i][x] );
-			sud->grid[y][x] &= ( sud->grid[y][x] ) & ( sud->grid[i][x] );
+			sud->grid[i][x] = ( ( sud->grid[y][x] ) & ( sud->grid[i][x] ) );
+			sud->grid[y][x] = ( ( sud->grid[y][x] ) & ( sud->grid[i][x] ) );
+			return 1;
 		}
 	}
-
-
 	return 0;
 }
