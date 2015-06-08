43f2e80a9cba43a94122c8b90c5b35a6441d7ecb
MMauser <Schmitz.marco@gmx.de>
2015-06-08 20:23:17 +0200

    added rule4

--- solver/solver/rule4.c
+++ solver/solver/rule4.c
@@ -2,5 +2,26 @@
 
 //beschreibung
 int rule4( struct Sudoku* sud, unsigned int x, unsigned int y ) {
+	unsigned int i;
+	SudokuCell col;
+
+	col = 0;
+	for ( i = 0; i < sud->length; i++ )
+	{
+		if ( i != x )
+		{
+			col |= sud->grid[y][i];
+		}
+	}
+
+	for ( i = 0; i < sud->length; i++ )
+	{
+		if ( __popcnt ( ( sud->grid[y][x] ) & ( sud->grid[y][i] ) ) == 2 && i != y )
+		{
+			sud->grid[y][x] = ( ( sud->grid[y][x] ) & ( sud->grid[y][i] ) );
+			sud->grid[y][x] = ( ( sud->grid[y][x] ) & ( sud->grid[y][i] ) );
+			return 1;
+		}
+	}
 	return 0;
 }
