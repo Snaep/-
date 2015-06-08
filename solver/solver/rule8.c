91f22635d96db25b45cc41d1410fe358aed761a9
MMauser <Schmitz.marco@gmx.de>
2015-06-08 21:35:14 +0200

    added rule8

--- solver/solver/rule8.c
+++ solver/solver/rule8.c
@@ -1,6 +1,29 @@
 #include "rules.h"
 
-//beschreibung
-int rule8( struct Sudoku* sud, unsigned int x, unsigned int y ) {
-	return 0;
+//Naked pairs box
+int rule8 ( struct Sudoku* sud, unsigned int x, unsigned int y )
+{
+	unsigned int i, j;
+	if ( __popcnt ( sud->grid[y][x] == 2 ) )
+	{
+		for ( i = 0; i < sud->length; i++ )
+		{
+			if ( ( sud->grid[y][x] ) ^ ( *sud->cellbox[y][x][i] ) && ( &( sud->grid[y][x] ) ) != sud->cellbox[y][x][i] )
+			{
+				for ( j = 0; j < sud->length; j++ )
+				{
+					if ( ( &( sud->grid[y][x] ) ) != sud->cellbox[y][x][j] )
+					{
+						*sud->cellbox[y][x][j] &= ( ~( sud->grid[y][x] ) );
+						return 1;
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+		return 0;
+	}
+
 }
