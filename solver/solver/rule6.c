987fb64b2ffdae4afeb4a8c3a5c358a159f05744
Snaep <ksnaep@web.de>
2015-06-08 21:28:07 -0700

    rule6

--- solver/solver/rule6.c
+++ solver/solver/rule6.c
@@ -2,5 +2,17 @@
 
 //beschreibung
 int rule6( struct Sudoku* sud, unsigned int x, unsigned int y ) {
+	int i, j;
+	if( __popcnt( sud->grid[y][x] ) != 2 ) return 0;
+
+	for( i = 0; i < sud->length; i++ ) {
+		if( i == x ) continue;
+		if( sud->grid[y][i] ^ sud->grid[y][x] == 0 ) {
+			for( j = 0; j < sud->length; j++ ) {
+				if( j != i && j != x ) sud->grid[y][j] &= stuff;
+			}
+		}
+	}
+
 	return 0;
 }
