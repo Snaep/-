2cf99f83939fa0049e666d1cf6231162a3db1e70
Snaep <ksnaep@web.de>
2015-06-10 21:57:36 -0700

    r9

--- solver/solver/rule9.c
+++ solver/solver/rule9.c
@@ -1,6 +1,30 @@
 #include "rules.h"
 
-//fuk dis
+//naked subset column
 int rule9( struct Sudoku* sud, unsigned int x, unsigned int y ) {
+	unsigned int i, j, changed, subset, ctPartners;
+	unsigned int partners[4];
 
+	for( subset = 4; subset >= 3; subset-- ) {
+		if( __popcnt64( sud->grid[y][x] ) != subset ) continue;
+
+		ctPartners = 0;
+		for( i = 0; i < sud->length; i++ ) {
+			if( i != y && ( (sud->grid[i][x] & ( ~sud->grid[y][x] ) ) == 0 ) ) {
+				partners[ctPartners++] = i;
+			}
+		}
+
+		if( ctPartners != subset ) continue;
+
+		changed = 0;
+		for( i = 0; i < sud->length; i++ ) {
+			changed |= ( sud->grid[i][x] & ( ~sud->grid[y][x] ) );
+			sud->grid[i][x] &= ( ~sud->grid[y][x] );
+		}
+
+		return changed;
+	}
+
+	return 0;
 }
