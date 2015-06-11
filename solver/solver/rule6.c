2808b054fcd9e2604816795ec48e19fe1e8787e6
Snaep <ksnaep@web.de>
2015-06-10 18:45:20 -0700

    ll

--- solver/solver/rule6.c
+++ solver/solver/rule6.c
@@ -6,16 +6,14 @@ int rule6 ( struct Sudoku* sud, unsigned int x, unsigned int y )
 
 	for ( i = 0; i < sud->length; i++ )
 	{
-		if ( (__popcnt64 ( ( sud->grid[y][x] ) & ( sud->grid[i][x] ) ) ) == 2 && i != y )
+		if ( (__popcnt64 ( ( sud->grid[y][x] ) & ( sud->grid[i][x] ) ) ) == 2ll && i != y )
 		{
-			if ( __popcnt64 ( sud->grid[y][x] ) != 2 || __popcnt64 ( sud->grid[i][x] ) != 2 )
+			if ( __popcnt64 ( sud->grid[y][x] ) != 2ll || __popcnt64 ( sud->grid[i][x] ) != 2ll )
 			{
 				sud->grid[i][x] = ( ( sud->grid[y][x] ) & ( sud->grid[i][x] ) );
 				sud->grid[y][x] = ( ( sud->grid[y][x] ) & ( sud->grid[i][x] ) );
 
 				return 1;
-
-
 			}
 
 		}
