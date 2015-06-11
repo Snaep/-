82b9458011a084b57355d7cb3f5abd447aa58fa5
Snaep <ksnaep@web.de>
2015-06-10 18:22:13 -0700

    15

--- solver/solver/rule8.c
+++ solver/solver/rule8.c
@@ -5,7 +5,7 @@ int rule8( struct Sudoku* sud, unsigned int x, unsigned int y ) {
 	unsigned int i;
 
 	for( i = 0; i < sud->length; i++ ) {
-		if( ( __popcnt64( ( sud->grid[y][x] ) & ( *sud->cellbox[y][x][i] ) ) == 2ll ) && ( &sud->grid[y][x] != *sud->cellbox[y][x][i] ) ) {
+		if( ( __popcnt64( ( sud->grid[y][x] ) & ( *sud->cellbox[y][x][i] ) ) == 2ll ) && ( &sud->grid[y][x] != sud->cellbox[y][x][i] ) ) {
 			if( __popcnt64( sud->grid[y][x] ) != 2ll || __popcnt64( *sud->cellbox[y][x][i] ) != 2ll ) {
 				*sud->cellbox[y][x][i] = ( ( sud->grid[y][x] )&( *sud->cellbox[y][x][i] ) );
 				sud->grid[y][x] = ( ( sud->grid[y][x] )&( *sud->cellbox[y][x][i] ) );
