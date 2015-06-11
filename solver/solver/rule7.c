99626060d9c10d7a82e406e0a3eb4d22382ff057
Snaep <ksnaep@web.de>
2015-06-10 21:20:07 -0700

    cümüüüüüüt

--- solver/solver/rule7.c
+++ solver/solver/rule7.c
@@ -2,13 +2,13 @@
 
 //Hidden pairs in Reihen
 int rule7( struct Sudoku* sud, unsigned int x, unsigned int y ) {
-	unsigned int i, j;
+	unsigned int i, j, changed;
 	SudokuCell candidate, neighbourhood;
 
 	for( i = 0; i < sud->length; i++ ) {
 		candidate = ( sud->grid[y][x] ) & ( sud->grid[y][i] );
-		if( __popcnt64( candidate ) == 2 && i != x ) {
-			if( ( ( __popcnt64( sud->grid[y][x] ) != 2ll ) || ( __popcnt64( sud->grid[y][i] != 2ll ) ) ) != 0 ) {
+		if( __popcnt64( candidate ) == 2ll && i != x ) {
+			if( ( __popcnt64( sud->grid[y][x] ) != 2ll ) || ( __popcnt64( sud->grid[y][i] ) != 2ll ) ) {
 				neighbourhood = 0;
 				for( j = 0; j < sud->length; j++ ) {
 					if( j != i && j != x ) neighbourhood |= sud->grid[y][j];
